15375c68ed5e773a3e62e35b54754f6f457b83c8
BrmatsinJHB <Brmatsin@student.wethinkcode.co.za>
2020-07-15 15:21:02 +0200

    completed function

---
+++ ex01/ft_ultimate_ft.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_ft.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: brmatsin <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/13 16:37:38 by brmatsin          #+#    #+#             */
+/*   Updated: 2020/07/13 17:18:04 by brmatsin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<unistd.h>
+#include <stdio.h>
+
+void   ft_putchar(char c)
+{
+	   write(1, &c, 1);
+}
+
+void   ft_ultimate_ft(int *nbr1, int *nbr2, int *nbr3, int *nbr4, int *nbr5, int *nbr6, int *nbr7, int *nbr8, int *nbr9)
+{
+	  = (42);
+	  
+
+}
+
+int    main(void)
+{
+	int a;
+
+	a = 22;
+	ft_ultimate_ft(&a);
+	return 0;
+}
+
+
+
